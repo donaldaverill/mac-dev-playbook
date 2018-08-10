8da4933b46face48f6b11c0f33cccc09f3d7b7b4
Donald Averill <donaldaverill@gmail.com>
2018-08-09 19:01:40 -0700

    Update bootstrap_remote.sh

--- bootstrap_remote.sh
+++ bootstrap_remote.sh
@@ -81,11 +81,6 @@ cd ~/tmp_laptop/mac-dev-playbook
 mkdir ~/tmp_laptop/mac-dev-playbook/roles
 cd ~/tmp_laptop/mac-dev-playbook/roles
 
-fancy_echo "Cloning ansible-role-visual-studio-code-extensions repo ..."
-git clone https://github.com/donaldaverill/ansible-role-visual-studio-code-extensions.git
-
-mv ~/tmp_laptop/mac-dev-playbook/roles/ansible-role-visual-studio-code-extensions ~/tmp_laptop/mac-dev-playbook/roles/visual-studio-code-extensions
-
 cd ~/tmp_laptop/mac-dev-playbook
 cp config_template.yml config.yml
 
